6137d68f62f85f8bc40b343ca4c3466b179946cf
Rodney Lab <codesign@rodneylab.com>
2023-02-07 08:48:23 +0000

    fix: 💫 let user "login"

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,43 @@
 import { ABLY_API_KEY } from '$env/static/private';
-import { error as svelteKitError } from '@sveltejs/kit';
+import { Temporal } from '@js-temporal/polyfill';
+import { error as svelteKitError, fail, redirect } from '@sveltejs/kit';
 import Ably from 'ably';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async function load({ url }) {
+export const actions: Actions = {
+	login: async ({ cookies, request }) => {
+		const form = await request.formData();
+		const name = form.get('name');
+		if (typeof name === 'string' && name) {
+			let clientId = cookies.get('clientId');
+			if (!clientId) {
+				clientId = crypto.randomUUID();
+			}
+
+			cookies.set('session', JSON.stringify({ clientId, name }), {
+				path: '/',
+				expires: new Date(Temporal.Now.plainDateTimeISO().add({ hours: 2 }).toString()),
+				sameSite: 'lax',
+				httpOnly: true
+			});
+
+			throw redirect(303, '/');
+		}
+		return fail(400, { name, missing: true });
+	}
+};
+
+export const load: PageServerLoad = async function load({ cookies, url }) {
 	try {
-		const ably = new Ably.Realtime.Promise(ABLY_API_KEY);
-		const token = await ably.auth.requestToken({ clientId: "it's me" });
+		const session = cookies.get('session');
+		if (session) {
+			const { clientId, name } = JSON.parse(session);
 
-		return { token };
+			const ably = new Ably.Realtime.Promise(ABLY_API_KEY);
+			const token = await ably.auth.requestToken({ clientId });
+
+			return { name, token };
+		}
 	} catch (error: unknown) {
 		const { pathname } = url;
 		const message = `Error in server load function for ${pathname}: ${error as string}`;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
 	import { cleanupPresence, initialisePresence } from '$lib/utilities/realtime';
 	import type { Types } from 'ably';
+	import { Realtime } from 'ably';
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	let { channel, serviceStatus } = data;
-	$: ({ channel, serviceStatus } = data);
+	let { name, token } = data ?? {};
+	$: ({ name, token } = data ?? {});
 
+	let channel: Types.RealtimeChannelPromise | null = null;
 	let message: string;
 	let messages: Types.Message[] = [];
 	let presenceData: Types.PresenceMessage[] = [];
+	let serviceStatus = '';
 	let status: string;
 
 	onMount(async () => {
+		let serviceStatus = 'Offline';
+		const ably = new Realtime.Promise({
+			...token,
+
+			authCallback: () => {
+				/* todo(rodneylab): add token refresh logic */
+			}
+		});
+
+		await ably.connection.once('connected');
+		serviceStatus = 'Connected to ably';
+
+		channel = ably.channels.get('getting-started');
+
 		channel?.subscribe((message) => {
 			messages = [...(messages ?? []), message];
 		});
@@ -39,6 +56,13 @@
 </script>
 
 <h1>Sveltably</h1>
+{#if !name}
+	<form action="?/login" method="POST">
+		<label for="name">Name</label>
+		<input id="name" type="text" name="name" placeholder="Your login name" />
+		<button type="submit">Login</button>
+	</form>
+{/if}
 <form on:submit|preventDefault={sendMessage}>
 	<label for="message">Message</label>
 	<input bind:value={message} id="message" type="text" name="message" />
